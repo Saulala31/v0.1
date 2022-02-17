df453a9b417523a753c38dfb6cc4fd564faa1034
Saulėnas <73237821+Saulala31@users.noreply.github.com>
2022-02-18 00:03:33 +0200

    Update Uzduotis.cpp

--- Uzduotis.cpp
+++ Uzduotis.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <cstring>
+#include <algorithm>
+#include <cassert>
 
 using namespace std;
 
@@ -14,16 +17,20 @@ struct data{
 void ivestis(data& temp);
 void isvestis(data*);
 void add_mok(data *&root);
+string PavertimasDidziosiomisR(string s);
+bool Ar_tai_yra_skaicius(string str);
 
 int main(){
     data *root = NULL;
-    int sk = 1, k=0;
-    cout << "Mokiniu pridejimas"<<endl;
-    while (sk !=0){
-        cout << "jei norite prideti mokini rasykite: 1, jei ne rasykite: 0"<<endl;
+    int k=0;
+    string sk("T");
+    while (sk !="N"){
+        cout << "Jei norite prideti mokini rasykite: T, jei ne rasykite: N"<<endl;
         cin >> sk;
-            if (sk== 0) break;
-            else add_mok(*&root);
+          sk = PavertimasDidziosiomisR(sk);
+            if (sk=="N") break;
+            else if (sk=="T") add_mok(*&root);
+            else cout << "ivedete neteisinga reiksme" << endl;
     }
 
     isvestis(root);
@@ -35,20 +42,48 @@ int main(){
 void ivestis(data& temp){
     cout << "Iveskite varda: "; cin >> temp.vardas;
     cout << "Iveskite pavarde: "; cin >> temp.pavarde;
-    int n = 1;
-    cout << "Veskite mokinio pazymius. Noredami uzbaigti pazymiu vedima irasykite: 0"<<endl; 
-    while(n!=0){
+    string n("T");
+    cout << "Veskite mokinio pazymius"<< endl << "Pazymiai gali buti tik tarp 1-10"<<endl<<
+     "Noredami uzbaigti pazymiu vedima irasykite: B"<<endl; 
+    while(n!="B"){
         cin >> n;
-        if (n==0) break;
+        n = PavertimasDidziosiomisR(n);
+        if (n == "B") break;
+        else if (!Ar_tai_yra_skaicius(n)){
+          cout << "Jus ivedete ne skaiciu, bandykite vel :)"<<endl;
+        }
         else {
-            temp.paz[temp.paz_sk] = n;
-            temp.paz_sk++;
+          int x = stoi(n);
+          if (x > 10 or x<1) cout << "ivestas skaicius neatitinka kriterijaus"<<endl;
+          else {
+              temp.paz[temp.paz_sk] = x;
+              temp.paz_sk++;
+          }
         }
     }
-    cout << "iveskite egzamino pazymi: "; cin >> temp.egz;
+    n.assign("T");
+    cout << "iveskite egzamino pazymi: ";
+    while(!Ar_tai_yra_skaicius(n)){
+        cin >> n;
+        if (!Ar_tai_yra_skaicius(n)){
+          cout << "Jus ivedete ne skaiciu, bandykite vel :)"<<endl;
+        }
+        else {
+          int x = stoi(n);
+          if (x > 10 or x<1){
+             cout << "ivestas skaicius neatitinka kriterijaus"<<endl;
+             n.assign("T");
+          }
+          else {
+              temp.paz[temp.paz_sk] = x;
+              temp.paz_sk++;
+              break;
+          }
+        }
+    }
 }   
 
-void isvestis(data *root) {
+void isvestis(data *root){
   data *t = root;
   while (t) {
     cout << std::setw(20) << t->vardas << std::setw(20) << t->pavarde;
@@ -59,7 +94,7 @@ void isvestis(data *root) {
   cout << endl;
 }
 
-void add_mok(data *&root) {
+void add_mok(data *&root){
   if (root) {
     data *t = root;
     while (t->next) t = t->next;
@@ -75,4 +110,15 @@ void add_mok(data *&root) {
     //root->data = rand()%10;
     root->next = NULL;
   }
+}
+string PavertimasDidziosiomisR(string s){
+    transform(s.begin(), s.end(), s.begin(),
+                   [](unsigned char c){ return toupper(c); });
+    return s;
+}
+bool Ar_tai_yra_skaicius(string str) {
+   for (int i = 0; i < str.length(); i++)
+   if (isdigit(str[i]) == false)
+      return false;
+      return true;
 }
