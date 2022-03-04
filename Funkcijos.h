0ee50e09fd722fd49530879c569d3a01b08958e5
Saulėnas <73237821+Saulala31@users.noreply.github.com>
2022-03-04 19:54:14 +0200

    Add files via upload

---
+++ Funkcijos.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <cstring>
+#include <algorithm>
+#include <cassert>
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iterator>
+
+using std::cout; using std::cin; using std::string; using std::endl; using std::left; using std::fixed; using std::setprecision; using std::sort;
+using std:: vector; using std::setw; using std::ifstream;
+using std::cout; using std::cin;
+using std::endl; using std::string;
+using std::vector; using std::istringstream;
+using std::stringstream; using std::ofstream;
+
+struct data{
+    string vardas, pavarde;
+    int egz = 0, paz_sk = 0 , paz_suma = 0;
+    vector <int> paz;
+    double vid, med = 0;
+    vector <string> zod;
+};
+    void ivestis(vector<data>& temp);
+    void nuskaitymas(vector <data> &temp);
+    bool palyginimas(const data& a, const data& b);
+    void paskirstymas (vector <data> &temp);
+    string PavertimasDidziosiomisR(string s);
+    bool Ar_tai_yra_skaicius(string str);
+    double Mediana(vector <int> a);
+    void isvestis(vector<data> t);
+    string zodziu_sujungimas(string v, string v2);
