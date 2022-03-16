4f3563fbb8488370027f90428f63915f5dc77ecc
Saulėnas <73237821+Saulala31@users.noreply.github.com>
2022-03-16 22:31:42 +0200

    Update Uzduotis.cpp

--- Uzduotis.cpp
+++ Uzduotis.cpp
@@ -5,7 +5,8 @@ int main(){
     string sk("T");
     while (sk !="N"){
         cout << "Jei norite prideti mokini rasykite: T, jei ne rasykite: N"<<endl;
-        cout << "Jei norite, kad duomenys butu nuskaityti is failo rasykite: A" <<endl;
+        cout << "Duomenys nuskaitymui is failo rasykite: A" <<endl;
+        cout << "Failu generavimui rasykite: G"<<endl;
         cin >> sk;
         sk = PavertimasDidziosiomisR(sk);
         if (sk == "N") break;
@@ -31,6 +32,10 @@ int main(){
             nuskaitymas(mokinys, df);
             df.close();
             break;
+        }
+        else if (sk == "G"){
+                FailuGeneravimas();
+                break;
         }
         else cout << "ivedete neteisinga reiksme" << endl;
     }
